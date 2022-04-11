f1fc641d20f553ed10d372edd4a8d0f77b556cb6
Specifix <62927726+Specifix5@users.noreply.github.com>
2022-04-11 15:32:37 +0700

    dev.lua
    e

---
+++ dev.lua
@@ -0,0 +1,27 @@
+local user = "Target"
+local plr = game.Players.LocalPlayer.Name
+
+local can = true
+
+function getDestination()
+    -- Up to you to determine where this thing is going!
+    return workspace[user.."Aircraft"].PilotSeat.Seat.Position
+end
+
+workspace[plr].Humanoid.Died:Connect(function()
+	can = false
+end)
+
+while wait(.1) do
+	if can == true then
+		if workspace[plr.."Aircraft"].PilotSeat.Seat == nil then
+			workspace.Gravity = 196
+		else
+			workspace.Gravity = 0
+			local offset = getDestination() - workspace[plr.."Aircraft"].PilotSeat.Seat.Position
+			--workspace.Filename_03Aircraft.PilotSeat.Seat.CFrame = workspace[user.."Aircraft"].PilotSeat.Seat.CFrame * CFrame.new(0, 0, 80)
+			workspace[plr.."Aircraft"].PilotSeat.Seat.Velocity = offset.Unit * ((workspace[user.."Aircraft"].PilotSeat.Seat.Velocity).magnitude * 2)
+		end
+	end
+end
+--loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
