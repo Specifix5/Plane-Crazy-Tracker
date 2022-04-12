5a13ec12dec19321d54810a9c2ba56fcba21e638
Specifix <62927726+Specifix5@users.noreply.github.com>
2022-04-12 12:14:16 +0700

    Update dev.lua

--- dev.lua
+++ dev.lua
@@ -1,4 +1,5 @@
 local user = "Rahalmulit"
+local speed = 1250
 local plr = game.Players.LocalPlayer.Name
 
 function notify(title, text)
@@ -10,41 +11,17 @@ function notify(title, text)
 	})
 end
 
-notify("Ready", "Script successfully been executed.")
-
-local gui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
-gui.IgnoreGuiInset = true
-gui.DisplayOrder = 69420
-gui.ResetOnSpawn = true
-
-local tb = Instance.new("TextBox", gui)
-tb.Position = UDim2.new(0.424, 0, 0.012, 0)
-tb.Size = UDim2.new(0.151, 0, 0.035, 0)
-tb.BackgroundTransparency = 0.5
-tb.BackgroundColor3 = Color3.fromRGB(0,0,0)
-tb.TextColor3 = Color3.fromRGB(255,255,255)
-tb.PlaceholderText = "Enter a name here.."
-tb.Text = ""
-
-tbCon = tb:GetPropertyChangedSignal("Text"):Connect(function()
-	if game.Players:FindFirstChild(tb.Text) then
-		local pler = game.Players:GetPlayerFromCharacter(workspace[tb.Text])
-		print(pler)
-
-		if pler and pler.Character and workspace[pler.Name.."Aircraft"].PilotSeat.Seat ~= nil then
-			user = pler.Name
-			notify("Success", "Changed target to: "..pler.Name)
-		else
-			notify("Error", "Unable to find player vehicle")
-		end
-	end
-end)
+if workspace[user.."Aircraft"].PilotSeat.Seat.Position == nil then
+	notify("Error", "Unable to find player vehicle")
+else
+	notify("Ready", "Script successfully been executed.")
+end
 
 local can = true
 
 function getDestination()
     -- Up to you to determine where this thing is going!
-    return workspace[user.."Aircraft"].PilotSeat.Seat.Position
+    return workspace[user].HumanoidRootPart.Position--.."Aircraft"].PilotSeat.Seat.Position
 end
 
 workspace[plr].Humanoid.Died:Connect(function()
@@ -63,7 +40,7 @@ while wait(.1) do
 			workspace.Gravity = 0
 			local offset = getDestination() - workspace[plr.."Aircraft"].PilotSeat.Seat.Position
 			--workspace.Filename_03Aircraft.PilotSeat.Seat.CFrame = workspace[user.."Aircraft"].PilotSeat.Seat.CFrame * CFrame.new(0, 0, 80)
-			workspace[plr.."Aircraft"].PilotSeat.Seat.Velocity = offset.Unit * 1650--((workspace[user.."Aircraft"].PilotSeat.Seat.Velocity).magnitude * 1.25)
+			workspace[plr.."Aircraft"].PilotSeat.Seat.Velocity = offset.Unit * speed--((workspace[user.."Aircraft"].PilotSeat.Seat.Velocity).magnitude * 1.25)
 		end
 	end
 end
