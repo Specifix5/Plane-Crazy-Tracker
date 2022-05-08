a8413e52d01d0ea216c2ecfcef1956aa905394dd
Specifix <62927726+Specifix5@users.noreply.github.com>
2022-05-08 18:06:46 +0700

    Update dev.lua
    Update

--- dev.lua
+++ dev.lua
@@ -1,6 +1,39 @@
-local user = "TARGET HERE"
-local speed = 1250
+--[[ 
+	Hello there, whoever is using my script. Is it me Specifix a.k.a your local Filename_03, here's some 
+	basic introduction to using the script.
+
+	<!> PLEASE NOTE THAT DO NOT ADD ANY PHYSICS OBJECT INTO YOUR VEHICLE <!>
+	(e.g: Motors, Parachutes, Ropes, and many other!) Plane crazy's physics handler
+	will override the script's physics alternation ability (to move and do stuff)
+
+	The max speed for cutters to actully work and break blocks is around ~1250, so please remember that!
+	the faster you go, the more useless it gets.
+
+	If you are a toxic exploiter, you can enable "useAnnoyingChatMessages" for you to say stuff when
+	target dies, script executes (e.g: "LOL! You're being targetted.")
+
+	NOTE: There's this stupid ass bug where your ping sometimes commit unexist when you go too fast.
+
+
+	TODO:
+	- Add an exploiter detector, potentially detects an exploiter and marks them as suspicious.
+	This helps you to know that there's a physics altering exploiter such as vfly (vehicle fly)
+	user and stuff. Might wanna go for that hacker vs hacker 1v1 moment
+
+	- Priority List, A blacklist sort of thing to make you target the blacklisted user available in blacklist.txt
+--]]
+
+local user = "TARGET HERE" -- Target User here
+local speed = 1250 -- Specified Speed the vehicle will move at
 local plr = game.Players.LocalPlayer.Name
+
+-- Settings
+local useAnnoyingChatMessages = false -- Enable Chat messages (e.g: "LOL! You're being targetted.")
+
+local targetDiedEvent
+
+-- IGNORE THIS -- Wether the script continue to execute and update velocity!
+local can = true
 
 function notify(title, text)
 	game.StarterGui:SetCore("SendNotification", {
@@ -11,13 +44,48 @@ function notify(title, text)
 	})
 end
 
-if workspace[user.."Aircraft"].PilotSeat.Seat.Position == nil then
-	notify("Error", "Unable to find player vehicle")
-else
-	notify("Ready", "Script successfully been executed.")
+function playAudio(audioId)
+	local s = Instance.new("Sound", workspace)
+	s.SoundId = "rbxassetid://"..audioId
+	s.Volume = 1.25
+	s.PlayOnRemove = true
+
+	wait()
+
+	s:Destroy()
+	print("Successfully played audio:"..audioId)
 end
 
-local can = true
+if workspace:FindFirstChild(user.."Aircraft") == nil then
+	playAudio(9576185430)
+	notify("Error", "Unable to find player vehicle")
+	return
+else
+	notify("Ready", "Script successfully been executed.")
+	playAudio(9576178484)
+	if useAnnoyingChatMessages then
+		game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("LOL! You're being targetted, "..user, "All")
+	end
+
+	targetDiedEvent = workspace[user].Humanoid.Died:Connect(function ()
+		playAudio(9576182449)
+		if useAnnoyingChatMessages then
+			game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("L You died, imagine! "..user, "All")
+		end
+
+		can = false
+		wait(game.Players.RespawnTime + 3.5)
+
+		repeat wait() until workspace:FindFirstChild(user.."Aircraft") and workspace:FindFirstChild(plr.."Aircraft")
+
+		playAudio(9576428703)
+		notify("Target Respawned", "Maintaining Velocity to target's position")
+		
+		can = true
+	end)
+end
+
+can = true
 
 function getDestination()
     -- Up to you to determine where this thing is going!
@@ -26,7 +94,11 @@ end
 
 
 workspace[plr].Humanoid.Died:Connect(function()
+	playAudio(9576183844)
 	can = false
+	wait(.1)
+	targetDiedEvent:Disconnect()
+	targetDiedEvent = nil
 end)
 
 for i, v in pairs(workspace.BuildingZones:GetDescendants()) do
