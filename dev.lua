686b6541ec3c79a8d79ac2c27fdcb0fc0429b879
Specifix <62927726+Specifix5@users.noreply.github.com>
2022-05-28 09:15:16 +0700

    Added Automatic Shredder Detection System
    Identify Shredders Automatically and it will shows a BillboardGUI containing their usernames.
    ERROR: will detect anything as a "Shredder" if it has more than 5 Cutter blocks in the PlayerAircraft object.

--- dev.lua
+++ dev.lua
@@ -1,30 +1,23 @@
 --[[ 
 	Hello there, whoever is using my script. Is it me Specifix a.k.a your local Filename_03, here's some 
 	basic introduction to using the script.
-
 	<!> PLEASE NOTE THAT DO NOT ADD ANY PHYSICS OBJECT INTO YOUR VEHICLE <!>
 	(e.g: Motors, Parachutes, Ropes, and many other!) Plane crazy's physics handler
 	will override the script's physics alternation ability (to move and do stuff)
-
 	The max speed for cutters to actully work and break blocks is around ~1250, so please remember that!
 	the faster you go, the more useless it gets.
-
 	If you are a toxic exploiter, you can enable "useAnnoyingChatMessages" for you to say stuff when
 	target dies, script executes (e.g: "LOL! You're being targetted.")
-
 	NOTE: There's this stupid ass bug where your ping sometimes commit unexist when you go too fast.
-
-
 	TODO:
 	- Add an exploiter detector, potentially detects an exploiter and marks them as suspicious.
 	This helps you to know that there's a physics altering exploiter such as vfly (vehicle fly)
 	user and stuff. Might wanna go for that hacker vs hacker 1v1 moment
-
 	- Priority List, A blacklist sort of thing to make you target the blacklisted user available in blacklist.txt
 --]]
 
 local user = "TARGET" -- Target User here
-local speed = 2250 -- Specified Speed the vehicle will move at (It is recommended to not go above 1250)
+local speed = 3250 -- Specified Speed the vehicle will move at (It is recommended to not go above 1250)
 local plr = game.Players.LocalPlayer.Name
 
 -- Settings
@@ -34,6 +27,64 @@ local targetDiedEvent
 
 -- IGNORE THIS -- Wether the script continue to execute and update velocity!
 local can = true
+
+if not workspace:FindFirstChild("ExecutedOnce") then
+	_e = Instance.new("BoolValue", workspace)
+	_e.Name = "ExecutedOnce"
+
+	function MakeGui(name)
+		local l = Instance.new("BillboardGui", workspace[name].Head)
+		l.Name = "Sman"
+
+		l.Size = UDim2.new(0, 300, 0, 70)
+		l.StudsOffsetWorldSpace = Vector3.new(0, 1, 0)
+		l.AlwaysOnTop = true
+
+		local tx = Instance.new("TextLabel", l)
+		tx.BackgroundTransparency = 1
+		tx.Text = "<!> SHREDDERMAN ALERT <!>"
+		tx.Size = UDim2.new(0, 300, 0, 70)
+		tx.TextColor3 = Color3.fromRGB(255, 99, 99)
+		tx.Font = Enum.Font.SourceSansSemibold
+		tx.TextSize = 25
+		tx.TextStrokeTransparency = 0.7
+		tx.TextYAlignment = Enum.TextYAlignment.Top
+
+		tx = Instance.new("TextLabel", l)
+		tx.BackgroundTransparency = 1
+		tx.Text = tostring(name)
+		tx.Size = UDim2.new(0, 300, 0, 70)
+		tx.TextColor3 = Color3.fromRGB(255, 109, 25)
+		tx.Font = Enum.Font.SourceSansSemibold
+		tx.TextSize = 30
+		tx.TextStrokeTransparency = 0.7
+		tx.TextYAlignment = Enum.TextYAlignment.Center
+	end
+
+	workspace.ChildAdded:Connect(function (ch)
+		if ch.Name:match("Aircraft") and ch.Name ~= game.Players.LocalPlayer.Name.."Aircraft" then
+			local pl = workspace[ch.Name:gsub("Aircraft", "")]
+			print(pl)
+			wait(5)
+			local cutterFound = 0
+			for i, v in pairs(ch:GetDescendants()) do
+				if v.Name == "Cutter" then
+					cutterFound = cutterFound + 1
+				end
+			end
+			wait(.5)
+			if cutterFound > 5 then
+				if not (workspace[pl.Name].Head:FindFirstChild("Sman")) then
+					MakeGui(pl.Name)
+				else
+					workspace[pl.Name].Head:FindFirstChild("Sman"):Destroy()
+					wait()
+					MakeGui(pl.Name)
+				end
+			end
+		end
+	end)
+end
 
 function notify(title, text)
 	game.StarterGui:SetCore("SendNotification", {
